17fbc5f64d5edd7169c42da457fda025a382ffe9
Shyam Kannan <shmkanna@gmail.com>
2024-04-30 22:15:45 -0400

    Code Running Properly

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import population
 
 pygame.init() # Initializes Pygame(set up the display, etc.)
 clock = pygame.time.Clock() # Creates a clock object to control the frame rate
+population = population.Population(100) # Creates a population of 100 players
 
 def generate_pipes(): # Adds a new pipe to the list of pipes depending on the window width
-    gamereqs.pipes.append(gamereqs.Pipes(gamereqs.win_width))
+    gamereqs.pipes.append(gamereqs.Pipes(gamereqs.WINDOW_WIDTH))
     
 def quit_game(): # Quits the game properly if the user closes the window
     for event in pygame.event.get():
@@ -38,7 +39,13 @@ def main():
             i.update() # Updates the pipe's position
             if i.off_screen: # Removes the pipe from the list if it is off the window
                 gamereqs.pipes.remove(i)
-        
+                
+        if not population.extinct(): # Checks if the population is extinct
+            population.update_live_players() # Updates the live players
+        else:
+            gamereqs.pipes.clear()
+            population.natural_selection() # Starts the natural selection process
+            
         clock.tick(60) # Sets the frame rate to 60 frames per second
         pygame.display.flip() # Updates the display 
 
