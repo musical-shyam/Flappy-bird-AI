4b178292914f45577d5972b58008fa4e488e9232
doodjames <rrahman2004@gmail.com>
2024-05-01 17:25:59 -0400

    updated requirements to match what is used

--- neft-flappy-bird-master/config.py
+++
@@ -1,9 +0,0 @@
-import components
-import pygame #in built library
-
-win_height = 550
-win_width = 550
-window = pygame.display.set_mode((win_width, win_height))
-
-ground = components.Ground(win_width)
-pipes = []
