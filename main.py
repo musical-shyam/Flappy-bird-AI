7f6ec7c7dc3b53492e8d4ce5fff52da809dfe9cd
doodjames <rrahman2004@gmail.com>
2024-04-30 22:53:49 -0400

    added generation counter to the game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 from sys import exit
 import gamereqs
 import population
@@ -6,6 +7,7 @@ import population
 pygame.init() # Initializes Pygame(set up the display, etc.)
 clock = pygame.time.Clock() # Creates a clock object to control the frame rate
 population = population.Population(100) # Creates a population of 100 players
+font = pygame.font.Font('Roboto-Regular.ttf', 30)
 
 def generate_pipes(): # Adds a new pipe to the list of pipes depending on the window width
     gamereqs.pipes.append(gamereqs.Pipes(gamereqs.WINDOW_WIDTH))
@@ -16,6 +18,7 @@ def quit_game(): # Quits the game properly if the user closes the window
             pygame.quit()
             exit()
 def main():
+    generationCount = 0
     pipes_spawn_time = 10 # Time between each pipe spawn
     while True:
         # Checks if the user has closed the window
@@ -33,7 +36,11 @@ def main():
             pipes_spawn_time = 200
         pipes_spawn_time -= 1
         
-        
+        # Generation count output
+        gen_count_label = font.render("Generation: " + str(generationCount),1,(0,0,0))
+        gamereqs.window.blit(gen_count_label, (10, 510))
+
+
         for i in gamereqs.pipes:
             i.draw(gamereqs.window) # Draws the pipe on the window
             i.update() # Updates the pipe's position
@@ -45,7 +52,8 @@ def main():
         else:
             gamereqs.pipes.clear()
             population.natural_selection() # Starts the natural selection process
-            
+            generationCount += 1
+
         clock.tick(60) # Sets the frame rate to 60 frames per second
         pygame.display.flip() # Updates the display 
 
